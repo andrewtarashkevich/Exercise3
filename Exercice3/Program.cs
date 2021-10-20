e5230a0eabcb82113db546fa455388cfc60e810e
Andrew <Andrew@HOME-PC>
2021-10-20 23:30:51 +0300

    Add project files.

---
+++ Exercice3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.1
+            Console.Write("Введите радиус: ");
+            double r = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину: ");
+            double l = double.Parse(Console.ReadLine());
+            Console.Write($"Площадь конуса = {Math.Round((Math.PI * r * (r + l)),3)}");
+            Console.WriteLine();
+
+            //Задание 1.2
+            int x = 14, y = 1, z = 5;
+            Console.WriteLine(x += y - x++ * z);
+            Console.WriteLine(z = --x - y * 5);
+            Console.WriteLine(y /= x + 5 % z);
+            Console.WriteLine(z = x++ + y * 5);
+            Console.WriteLine(x = y - x++ * z);
+
+        }
+    }
+}
---
+++ NOD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
